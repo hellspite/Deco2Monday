27f7ce04a17a43561c0bfec9689c9d315a492fb7
Emiliano Nobis <emiliano.nobis@gmail.com>
2022-02-28 23:04:28 +0100

    Deco environment variables

--- deco.py
+++ deco.py
@@ -1,28 +1,33 @@
 # Manages the Deco part of the app
 
 import requests
+import os
 from datetime import datetime, date, timedelta
 
-USERNAME = ""
-PASSWORD = ""
+USERNAME = os.environ["DECO_USER"]
+PASSWORD = os.environ["DECO_PASS"]
 API_URL = "https://straighttohell.eu/api/json/manage_orders/find"
 
-# Retrieve yesterday's and today's date at midnight
-yesterday = date.today() - timedelta(days=1)
-yesterday_formatted = yesterday.strftime("%Y-%m-%dT00:00:00")
-today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-today_formatted = today.strftime("%Y-%m-%dT%H:%M:%S")
 
-params = {
-    "field": "1",
-    "condition": "6",
-    "date1": yesterday_formatted,
-    "username": USERNAME,
-    "password": PASSWORD
-}
+def get_orders():
 
-print(yesterday_formatted)
+    # Retrieve yesterday's and today's date at midnight
+    yesterday = date.today() - timedelta(days=1)
+    yesterday_formatted = yesterday.strftime("%Y-%m-%dT00:00:00")
+    today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+    today_formatted = today.strftime("%Y-%m-%dT%H:%M:%S")
 
-response = requests.get(API_URL, params=params)
+    # TODO if it's monday select orders from two days ago
+    # TODO if it's midweek select orders from yesterday
 
-print(response.json()["orders"])
+    params = {
+        "field": "1",
+        "condition": "6",
+        "date1": yesterday_formatted,
+        "username": USERNAME,
+        "password": PASSWORD
+    }
+
+    response = requests.get(API_URL, params=params)
+
+    print(response.json()["orders"])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Python script to export orders from DecoNetwork into Monday tasks
 
+import deco
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    deco.get_orders()
