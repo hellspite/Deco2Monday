f267d08cb0ee31f0ceca7ab186899e86f0e38bb1
Emiliano Nobis <vegan.emiliano@gmail.com>
2025-12-09 16:32:35 +0100

    Research only 14 days ahead

--- terminal.py
+++ terminal.py
@@ -7,7 +7,7 @@ import monday
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    days_back = 60
+    days_back = 14
     no_write = False
 
     if len(sys.argv) > 1:
